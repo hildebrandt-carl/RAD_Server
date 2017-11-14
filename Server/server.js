2bd9ec9ff88f9f798854cb4d1f7175850d391c97
Carl Hildebrandt <hildebrandt.carl@outlook.com>
2017-11-14 16:52:46 -0600

    Changed the webpage to say hellow world

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,4 @@
-var http = require("http");
-
-
+/*var http = require("http");
 
 http.createServer(function (request, response) {
 
@@ -13,4 +11,23 @@ request.on("end", function () {
     response.end('Hello HTTP!');
 });
 
-}).listen(4242);
+}).listen(4242);*/
+
+
+var http = require("http");
+var server = http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("<!DOCTYPE html>");
+  response.write("<html>");
+  response.write("<head>");
+  response.write("<title>Hello World Page</title>");
+  response.write("</head>");
+  response.write("<body>");
+  response.write("Hello World!");
+  response.write("</body>");
+  response.write("</html>");
+  response.end();
+});
+
+server.listen(4242);
+console.log("Server is listening");
