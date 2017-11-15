45046b9dd0836758aa6404624b8f066637745b92
Carl Hildebrandt <hildebrandt.carl@outlook.com>
2017-11-14 19:26:31 -0600

    Added the server code to get a message, and respond with -You said : (Whatever it recieved)-. I have also included client code to test the server

--- Server/server.js
+++ Server/server.js
@@ -1,4 +1,4 @@
-var http = require("http");
+/*var http = require("http");
 
 http.createServer(function (request, response) {
 
@@ -11,7 +11,7 @@ request.on("end", function () {
 });
 
     response.end('Seconds HTTP!');
-}).listen(4242);
+}).listen(4242);*/
 
 
 /*var http = require("http");
@@ -31,3 +31,48 @@ var server = http.createServer(function(request, response) {
 
 server.listen(4242);
 console.log("Server is listening");*/
+
+
+/*
+// server
+require('net').createServer(function (socket) {
+    console.log("connected");
+
+    socket.on('data', function (data) {
+        console.log(data.toString());
+    });
+})
+
+.listen(4242);*/
+
+
+
+var net = require('net');
+
+var PORT = 4242;
+
+// Create a server instance, and chain the listen function to it
+// The function passed to net.createServer() becomes the event handler for the 'connection' event
+// The sock object the callback function receives UNIQUE for each connection
+net.createServer(function(socket) {
+    
+    // We have a connection - a socket object is assigned to the connection automatically
+    console.log('CONNECTED: ' + socket.remoteAddress +':'+ socket.remotePort);
+    
+    // Add a 'data' event handler to this instance of socket
+    socket.on('data', function(data) {
+        
+        console.log('DATA ' + socket.remoteAddress + ': ' + data);
+        // Write the data back to the socket, the client will receive it as data from the server
+        socket.write('You said "' + data + '"');
+        
+    });
+    
+    // Add a 'close' event handler to this instance of socket
+    socket.on('close', function(data) {
+        console.log('CLOSED: ' + socket.remoteAddress +' '+ socket.remotePort);
+    });
+    
+}).listen(PORT)
+
+console.log('Server listening on port: ' + PORT);
